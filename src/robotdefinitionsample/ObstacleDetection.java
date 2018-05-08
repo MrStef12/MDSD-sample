e6f55d281b71a4fd667da739115e6d556ecea9ff
Steffen Hansen <stef.hansen@live.dk>
2018-05-08 13:22:19 +0200

    Added getShelfAtPos method

--- src/robotdefinitionsample/ObstacleDetection.java
+++ src/robotdefinitionsample/ObstacleDetection.java
@@ -8,13 +8,14 @@ package robotdefinitionsample;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 import robotdefinitionsample.models.Obstacle;
+import robotdefinitionsample.models.Shelf;
 import robotdefinitionsample.models.Vector2;
 
 /**
  *
  * @author stefh
  */
-public class ObstacleDetection {
+public class ObstacleDetection<T> {
     public static boolean detect(GridPane grid, DesiredProps props) {
         int x = props.getPos().getX();
         int y = props.getPos().getY();
@@ -30,12 +31,17 @@ public class ObstacleDetection {
         return false;
     }
     
-    private static Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
-    for (Node node : gridPane.getChildren()) {
-        if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
-            return node;
+    public static Shelf getShelfAtPos(GridPane grid, DesiredProps props) {
+        int x = props.getPos().getX();
+        int y = props.getPos().getY();
+        for(Node node : grid.getChildren()) {
+            if (node instanceof Shelf) {
+                Vector2 pos = ((Shelf) node).getPos();
+                if (pos.getX() == x && pos.getY() == y) {
+                    return (Shelf) node;
+                }
+            }
         }
+        return null;
     }
-    return null;
-}
 }
