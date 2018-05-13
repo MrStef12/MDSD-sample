50cd08e9c1cf14da62aa73cb2f2398dee94a7511
Steffen Hansen <stef.hansen@live.dk>
2018-05-13 14:26:52 +0200

    Added missing interface

---
+++ src/robotdefinitionsample/models/IConditionTasks.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotdefinitionsample.models;
+
+import java.util.List;
+
+/**
+ *
+ * @author stefh
+ */
+public interface IConditionTasks {
+    public void addTasks(List<TaskItem> items);
+}
